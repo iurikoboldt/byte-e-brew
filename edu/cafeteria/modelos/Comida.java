5df444e768a938218e88ffd458ad2f74f5490a0c
AnaBiaGuerra <biaguerra.ana@gmail.com>
2026-06-22 20:33:51 -0300

    Update Comida.java

--- edu/cafeteria/modelos/Comida.java
+++ edu/cafeteria/modelos/Comida.java
@@ -1,10 +1,27 @@
-package modelos;
-
+package br.edu.cafeteria.modelo;
 public class Comida extends Produto {
 
-    public Comida(String nome, int preco) {
-        super(nome, preco);
-        
+    private int tempoPreparo;
+    private boolean vegano;
+    private boolean semGluten;
+
+    public Comida(String codigo, String nome, double precoBase, int estoque, int tempoPreparo, boolean vegano, boolean semGluten){
+        super(codigo, nome, precoBase, estoque);
+        this.tempoPreparo = tempoPreparo;
+        this.vegano = vegano;
+        this.semGluten = semGluten;
     }
-    
+
+    public int getTempoPreparo() {
+        return tempoPreparo;
+    }
+
+    public boolean isVegano() {
+        return vegano;
+    }
+
+    public boolean isSemGluten() {
+        return semGluten;
+    }
 }
+

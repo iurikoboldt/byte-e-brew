047e5186d22604c82e1d61f350f5540c716a5fae
AnaBiaGuerra <biaguerra.ana@gmail.com>
2026-06-22 20:33:29 -0300

    Update Produto.java

--- edu/cafeteria/modelos/Produto.java
+++ edu/cafeteria/modelos/Produto.java
@@ -1,19 +1,51 @@
-package modelos;
-import servico.IPromocional;
+package br.edu.cafeteria.modelo;
+public abstract class Produto {
 
+    private String codigo;
+    private String nome;
+    private double precoBase;
+    private int estoque;
 
-public class Produto {
-        private int tempoDePreparo;
-        private double preco;
-        private int codigoIdentificador;
-        private String nome;
-
-        public Produto(String nome, int preco){
-            this.nome = nome;
-            this.preco = preco;
-        }
-        
-        
-
+    public Produto(String codigo, String nome, double precoBase, int estoque) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.precoBase = precoBase;
+        this.estoque = estoque;
     }
+
+    public boolean temEstoque(int quantidade){
+        return estoque >= quantidade;
+    }
+
+    public void reduzirEstoque(int quantidade) {
+        if (quantidade > estoque) {
+            estoque -= quantidade;
+        }
+    }
+    public void aumentarEstoque(int quantidade){
+        estoque -= quantidade;
+    }
+    public double getPrecoBase(){
+        return precoBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public int getEstoque(){
+
+        return estoque;
+    }
+    public String getCodigo (){
+        return codigo;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setPrecoBase(double precoBase){
+        this.precoBase = precoBase;
+    }
+}
 

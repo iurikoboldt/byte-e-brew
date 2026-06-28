ffe06b265ce97858e6120770116ccb12a600ad00
AnaBiaGuerra <biaguerra.ana@gmail.com>
2026-06-28 19:33:03 -0300

    Update IPromocional.java

--- br/edu/cafeteria/servico/IPromocional.java
+++ br/edu/cafeteria/servico/IPromocional.java
@@ -1,10 +1,10 @@
-package br.edu.cafeteria.servico;
+package br.edu.cafeteria.servico; // ou br.edu.cafeteria.servico, dependendo de onde o guardou
 
 import java.util.List;
+import br.edu.cafeteria.modelo.Produto;
+import br.edu.cafeteria.servico.Pedido;
 
-import br.edu.cafeteria.modelos.Produto;
-
-
-public interface IPromocional {
-    public double aplicarDescontoEventoGeek(Pedido pedido, List<Produto> lista);
+public interface Promocional {
+    // Verifique se o método recebe o Pedido e a Lista de Produtos:
+    double aplicarDescontoEventoGeek(Pedido pedido, List<Produto> produtos);
 }

9b0ca8a8c22a7f6cc21b323d85a1312e7fa7e80a
AnaBiaGuerra <biaguerra.ana@gmail.com>
2026-06-28 19:21:55 -0300

    Update Bebida.java

--- br/edu/cafeteria/modelos/Bebida.java
+++ br/edu/cafeteria/modelos/Bebida.java
@@ -1,26 +1,31 @@
-package br.edu.cafeteria.modelos;
+package br.edu.cafeteria.modelo;
 
-public class Bebida extends Produto {
-
-    private String tamanho;
+public class Bebida extends Produto{
+    private TamanhoBebida tamanho;
     private double cafeinaMg;
 
-    public Bebida(String codigo, String nome, double precoBase, int estoque, String tamanho, double cafeinaMg){
+    public Bebida(String codigo, String nome, double precoBase, int estoque, TamanhoBebida tamanho, double cafeinaMg){
         super(codigo, nome, precoBase, estoque);
         this.tamanho = tamanho;
         this.cafeinaMg = cafeinaMg;
+    }
 
-    }
-    public String getTamanho() {
-        return tamanho;
-    }
+    public TamanhoBebida getTamanho() {
+        return tamanho;}
 
     public double getCafeinaMg() {
-        return cafeinaMg;
-    }
+        return cafeinaMg;}
+
+
 
     @Override
     public void aplicarPromocao() {
 
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s (%s) - R$ %.2f | Cafeína: %.0fmg (Estoque: %d)",
+                getCodigo(), getNome(), tamanho.getDesc(), getPrecoBase(), cafeinaMg, getEstoque());
     }
 }
